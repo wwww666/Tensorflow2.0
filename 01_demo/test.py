07c4454f1c22ca9d607919cf6a931ea958aa89ee
wwww666 <57895751+wwww666@users.noreply.github.com>
2020-04-23 13:03:26 +0800

    Update test.py

--- 01_demo/test.py
+++ 01_demo/test.py
@@ -1,15 +1,1 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
 
-class Solution:
-    def deleteDuplicates(head):
-        if not head:return 0
-        a=head
-        while a and a.next:
-            if a.val==a.next.val:
-                a.next=a.next.next
-            else:
-                a=a.next
-        return head
